015954588ee4915ec74eda385f0acffcaba450f1
evilkry <39535614+evilkry@users.noreply.github.com>
2021-01-20 06:44:09 -0600

    Create local.sh

---
+++ etc/rc.local/local.sh
@@ -0,0 +1,42 @@
+#!/bin/sh ++group=host/vim/vmvisor/boot
+# EDITED 01/20/2021
+# local configuration options
+
+vusb0_status=$(esxcli network nic get -n vusb0 | grep 'Link Status' | awk '{print $NF}')
+count=0
+while [[ $count -lt 20 && "${vusb0_status}" != "Up" ]]
+do
+    sleep 10
+    count=$(( $count + 1 ))
+    vusb0_status=$(esxcli network nic get -n vusb0 | grep 'Link Status' | awk '{print $NF}')
+done
+
+if [ "${vusb0_status}" = "Up" ]; then
+
+    esxcfg-vswitch -L vusb0 iSCSI
+
+    esxcfg-vswitch -M vusb0 -p "iscsi" iSCSI
+
+# Note: create portgroups
+
+esxcli network vswitch standard portgroup add --portgroup-name=iscsi --vswitch-name=iSCSI
+
+# Note: create vSwitches
+
+esxcli network vswitch standard uplink add --uplink-name=vusb0 --vswitch-name=iSCSI
+
+fi
+
+
+
+#esxcfg-vswitch -R
+
+# Note: modify at your own risk!  If you do/use anything in this
+# script that is not part of a stable API (relying on files to be in
+# specific places, specific tools, specific output, etc) there is a
+# possibility you will end up with a broken system after patching or
+# upgrading.  Changes are not supported unless under direction of
+# VMware support.
+
+# Note: This script will not be run when UEFI secure boot is enabled.
+exit 0
